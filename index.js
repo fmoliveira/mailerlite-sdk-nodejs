bd8d3b66440559b765ad370d0388dda6f261e5fe
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-11 22:32:27 -0200

    Big revamp on module structure.

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
 'use strict';
 
-var request = require('request');
+/* Load RESTful client wrapper. */
+var Client = require('./lib/client');
 
-var Config = require('./config');
-var Campaigns = require('./lib/campaigns');
-var Lists = require('./lib/lists');
-var Subscribers = require('./lib/subscribers');
+/* Initialiser. */
+function MailerLite(apiKey) {
+  self.ApiKey = apiKey || process.env.MAILERLITE_API_KEY;
+};
 
+/* Register submodules. */
+MailerLite.prototype.Client = require('./lib/client');
+MailerLite.prototype.Campaigns = require('./api/campaigns');
+MailerLite.prototype.Lists = require('./api/lists');
+MailerLite.prototype.Subscribers = require('./api/Subscribers');
+
+/* Export the library. */
 module.exports = MailerLite;
-
-function MailerLite (apiKey) {
-  let self = this;
-
-  let credentials = {
-    apiKey: apiKey || process.env.MAILERLITE_API_KEY
-  };
-
-  let api = new ApiClient(credentials);
-
-  self.Campaigns = new Campaigns(api);
-
-  self.Lists = new Lists(api);
-
-  self.Subscribers = new Subscribers(api);
-}
-
-function ApiClient (credentials) {
-  let self = this;
-
-  /* Initialise client. */
-  let client = (verb, method, cb) => {
-    let uri = `${Config.url}${method}?apiKey=${credentials.apiKey}`;
-    return request(uri, cb);
-  };
-
-  /* Returns a promise to a given verb. */
-  let promise = (verb, method) => {
-    let promise = new Promise((resolve, reject) => {
-
-      let cb = (err, res, body) => {
-        let data = JSON.parse(body);
-
-        if (err) {
-          reject(err);
-        } else if (data && data.error) {
-          reject(data.error);
-        } else {
-          resolve(data);
-        }
-      };
-
-      client(verb, method, cb);
-    });
-    return promise;
-  };
-
-  /* API promises. */
-  self.get = (method) => promise('get', method);
-}
