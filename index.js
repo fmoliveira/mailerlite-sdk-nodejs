1a4819c91d479d2a4246c385e7e9ce6a44cd88e9
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-11 22:07:31 -0200

    Fixed indenting.

--- index.js
+++ index.js
@@ -2,59 +2,59 @@
 
 var request = require('request');
 
-var Config = require('./config'),
-    Campaigns = require('./lib/campaigns'),
-    Lists = require('./lib/lists'),
-    Subscribers = require('./lib/subscribers');
+var Config = require('./config');
+var Campaigns = require('./lib/campaigns');
+var Lists = require('./lib/lists');
+var Subscribers = require('./lib/subscribers');
 
 module.exports = MailerLite;
 
 function MailerLite (apiKey) {
-    let self = this;
+  let self = this;
 
-    let credentials = {
-        apiKey: apiKey || process.env.MAILERLITE_API_KEY
-    };
+  let credentials = {
+    apiKey: apiKey || process.env.MAILERLITE_API_KEY
+  };
 
-    let api = new ApiClient(credentials);
+  let api = new ApiClient(credentials);
 
-    self.Campaigns = new Campaigns(api);
+  self.Campaigns = new Campaigns(api);
 
-    self.Lists = new Lists(api);
+  self.Lists = new Lists(api);
 
-    self.Subscribers = new Subscribers(api);
+  self.Subscribers = new Subscribers(api);
 }
 
 function ApiClient (credentials) {
-    let self = this;
+  let self = this;
 
-    /* Initialise client. */
-    let client = (verb, method, cb) => {
-        let uri = `${Config.url}${method}?apiKey=${credentials.apiKey}`;
-        return request(uri, cb);
-    };
+  /* Initialise client. */
+  let client = (verb, method, cb) => {
+    let uri = `${Config.url}${method}?apiKey=${credentials.apiKey}`;
+    return request(uri, cb);
+  };
 
-    /* Returns a promise to a given verb. */
-    let promise = (verb, method) => {
-        let promise = new Promise((resolve, reject) => {
+  /* Returns a promise to a given verb. */
+  let promise = (verb, method) => {
+    let promise = new Promise((resolve, reject) => {
 
-            let cb = (err, res, body) => {
-                let data = JSON.parse(body);
+      let cb = (err, res, body) => {
+        let data = JSON.parse(body);
 
-                if (err) {
-                    reject(err);
-                } else if (data && data.error) {
-                    reject(data.error);
-                } else {
-                    resolve(data);
-                }
-            };
+        if (err) {
+          reject(err);
+        } else if (data && data.error) {
+          reject(data.error);
+        } else {
+          resolve(data);
+        }
+      };
 
-            client(verb, method, cb);
-        });
-        return promise;
-    };
+      client(verb, method, cb);
+    });
+    return promise;
+  };
 
-    /* API promises. */
-    self.get = (method) => promise('get', method);
+  /* API promises. */
+  self.get = (method) => promise('get', method);
 }
--- test/lists.js
+++ test/lists.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var expect = require('expect.js'),
-    MailerLite = require('..');
+var expect = require('expect.js');
+var MailerLite = require('..');
 
 describe('Lists', () => {
-    it('should return more than one list', (done) => {
-        var client = new MailerLite();
-        client.Lists.getAll()
-        .then((data) => {
-            expect(data).not.to.be(undefined);
-            expect(data).to.have.property('Results');
-            expect(data.Results.length).to.be.above(1);
-            done();
-        });
+  it('should return more than one list', (done) => {
+    var client = new MailerLite();
+    client.Lists.getAll()
+    .then((data) => {
+      expect(data).not.to.be(undefined);
+      expect(data).to.have.property('Results');
+      expect(data.Results.length).to.be.above(1);
+      done();
     });
+  });
 });
