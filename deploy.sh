2162deb1dd290aa574e2eccce70a02c3f9a278ac
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 02:04:05 +0000

    Simplifying deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,35 +3,28 @@
 set -e
 
 REPOSITORY="https://$GITHUB_DEPLOY_CREDENTIALS@github.com/fmoliveira/mailerlite-sdk-nodejs.git"
-OUTPUT_FOLDER=".docs"
 CONFIG_NAME=`git config user.name`
 CONFIG_EMAIL=`git config user.email`
 AUTHOR_NAME=${GITHUB_DEPLOY_AUTHOR_NAME:-$CONFIG_NAME}
 AUTHOR_EMAIL=${GITHUB_DEPLOY_AUTHOR_EMAIL:-$CONFIG_EMAIL}
 
 # Remove output folder if already being used
-rm -rf $OUTPUT_FOLDER
+rm -rf .docs .gh-pages
 
 # Clone GitHub pages
-git clone -b gh-pages --single-branch $REPOSITORY $OUTPUT_FOLDER
-
-# Remove current content
-git -C $OUTPUT_FOLDER rm -rf .
+git clone -b gh-pages --single-branch $REPOSITORY .gh-pages
 
 # Build documentation
 npm run make:docs
 
-# Add new content
-git -C $OUTPUT_FOLDER add --all .
+# Move documentation
+cd .gh-pages
+rm -rf *.*
+mv ../.docs/*.* .
 
-# Do commit
-git -C $OUTPUT_FOLDER commit -m "Deploy to GitHub pages." --author="$AUTHOR_NAME <$AUTHOR_EMAIL>"
+# Commit new content and push it
+git add --all .
+git commit -m "Deploy to GitHub pages." --author="$AUTHOR_NAME <$AUTHOR_EMAIL>"
+git log -1
+git push
 
-# Log commit
-git -C $OUTPUT_FOLDER log -1
-
-# Push
-git -C $OUTPUT_FOLDER push
-
-# Remove temporary folder
-rm -rf $OUTPUT_FOLDER
