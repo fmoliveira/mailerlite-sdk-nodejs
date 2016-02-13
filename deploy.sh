c32cdea9a60a7b271e7d72d3d59886c135269e9e
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 00:19:26 -0200

    Using Travis environment variables.

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,34 @@
 
 set -e
 
+if [ "$GITHUB_DEPLOY_CREDENTIALS" == "" ]; then
+  REPOSITORY_URL="git@github.com:$TRAVIS_REPO_SLUG.git"
+else
+  REPOSITORY_URL="https://$GITHUB_DEPLOY_CREDENTIALS@github.com/$TRAVIS_REPO_SLUG.git"
+fi
+
+echo "Repository: $REPOSITORY_URL"
+
 OUTPUT_FOLDER=".docs"
-REPOSITORY="https://$GITHUB_DEPLOY_CREDENTIALS@github.com/fmoliveira/mailerlite-sdk-nodejs.git"
 CONFIG_NAME=`git config user.name`
 CONFIG_EMAIL=`git config user.email`
 AUTHOR_NAME=${GITHUB_DEPLOY_AUTHOR_NAME:-$CONFIG_NAME}
 AUTHOR_EMAIL=${GITHUB_DEPLOY_AUTHOR_EMAIL:-$CONFIG_EMAIL}
+
+if [ "$TRAVIS_BUILD_NUMBER" == "" ]; then
+  COMMIT_MSG="Deploy to GitHub Pages."
+else
+  COMMIT_MSG="Automatic deployment to GitHub Pages via Travis CI build number #$TRAVIS_BUILD_NUMBER."
+fi
+
+echo $COMMIT_MSG
 
 # Remove output folder if already being used
 alias rimraf="rm -rf $OUTPUT_FOLDER"
 rimraf
 
 # Clone GitHub pages
-git clone -b gh-pages --single-branch $REPOSITORY $OUTPUT_FOLDER
+git clone -b gh-pages --single-branch $REPOSITORY_URL $OUTPUT_FOLDER
 
 # Alias to change Git working dir
 alias git="git -C $OUTPUT_FOLDER"
@@ -27,7 +42,7 @@ npm run make:docs
 
 # Commit new content and push it
 git add --all .
-git commit -m "Deploy to GitHub pages." --author="$AUTHOR_NAME <$AUTHOR_EMAIL>"
+git commit -m $COMMIT_MSG --author="$AUTHOR_NAME <$AUTHOR_EMAIL>"
 git log -1
 git push
 
