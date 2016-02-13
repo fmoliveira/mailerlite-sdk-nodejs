377d60608a0d1658fad10dbea69e6792320be840
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 10:27:09 -0200

    Documented adding a subscriber.

--- api/subscribers.js
+++ api/subscribers.js
@@ -9,9 +9,15 @@ module.exports = Subscribers;
  */
 function Subscribers () {
   /**
-   * Add subscriber.
+   * Adds a subscriber to an existing list, including custom field data if supplied.
+   * If the email address is already subscribed, their name and any custom field values are updated with whatever is passed in.
+   * @param {number} id - The ID of the list to which the subscriber should be added.
+   * @param {string} email - The email of the subscriber.
+   * @param {string} name - Name of the subscriber.
+   * @param {object[]} fields - Array of custom fields of the subscriber.
+   * @param {boolean} resubscribe - Sets if you want to reactive subscriber.
    */
-  this.addSubscriber = () => {
+  this.addSubscriber = (id, email, name, fields, resubscribe) => {
     //
   };
 
