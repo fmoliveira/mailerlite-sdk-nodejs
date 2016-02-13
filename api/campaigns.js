123fb6ac586db2d7bef197447509bf11eb6ed23e
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 10:19:31 -0200

    Documented campaign opens.

--- api/campaigns.js
+++ api/campaigns.js
@@ -36,7 +36,10 @@ function Campaigns () {
   };
 
   /**
-   * Get opens.
+   * Retrieves a paged result representing all the subscribers that opened a given campaign.
+   * @param {number} id - The ID of the campaign you want the stats for.
+   * @param {number} [limit] - Sets the limit of results in one page (default 1000).
+   * @param {number} [page] - You can navigate through records by increasing page number.
    */
   this.getOpens = () => {
     //
