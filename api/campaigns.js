c7d2f10b8ae9d1db0cc6f67ae9b16e4d8eca308d
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 10:19:02 -0200

    Documented campaign recipients.

--- api/campaigns.js
+++ api/campaigns.js
@@ -26,9 +26,12 @@ function Campaigns () {
   };
 
   /**
-   * Get recipients.
+   * Retrieves a paged result representing all the subscribers that a given campaign was sent to.
+   * @param {number} id - The ID of the campaign you want the stats for.
+   * @param {number} [limit] - Sets the limit of results in one page (default 1000).
+   * @param {number} [page] - You can navigate through records by increasing page number.
    */
-  this.getRecipients = () => {
+  this.getRecipients = (id, limit, page) => {
     //
   };
 
