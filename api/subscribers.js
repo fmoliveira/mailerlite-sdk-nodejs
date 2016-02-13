0e9ed576f24983ce2e9b72f3311559025b128aeb
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 10:29:35 -0200

    Documented adding many subscribers.

--- api/subscribers.js
+++ api/subscribers.js
@@ -22,9 +22,12 @@ function Subscribers () {
   };
 
   /**
-   * Add many subscribers.
+   * Allows you to add many subscribers to a list in one request, including custom field data if supplied.
+   * If an email addresses are already subscribed, their name and any custom field values are updated with whatever is passed in.
+   * @param {object[]} subscribers - Array of subscribers.
+   * @param {boolean} resubscribe - Sets if you want to reactive subscribers.
    */
-  this.addManySubscribers = () => {
+  this.addManySubscribers = (subscribers, resubscribe) => {
     //
   };
 
