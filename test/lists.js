752077139118e0b3ab57292eb8f79e42b7e15645
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-13 10:56:29 -0200

    Fixed indenting.

--- test/lists.js
+++ test/lists.js
@@ -8,11 +8,11 @@ var Client = new MailerLite();
 describe('Lists', () => {
   it('should return an array', (done) => {
     Client.Lists.getAll()
-    .then((data) => {
-      expect(data).not.to.be(undefined);
-      expect(data).to.have.property('Results');
-      expect(data.Results).to.be.an('array');
-      done();
-    });
+      .then((data) => {
+        expect(data).not.to.be(undefined);
+        expect(data).to.have.property('Results');
+        expect(data.Results).to.be.an('array');
+        done();
+      });
   });
 });
