a418767b79f90dc87b065f4a5f7e261b6b353c2f
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-28 17:18:48 -0300

    Test for list rename.

--- test/lists.js
+++ test/lists.js
@@ -8,6 +8,7 @@ var MailerLite = require('..')
 var ML = new MailerLite()
 
 const LIST_NAME = 'Mocha Test'
+const LIST_RENAME = 'Mocha Test After Rename'
 
 describe('Lists', () => {
   it('should return an array', (done) => {
@@ -25,6 +26,19 @@ describe('Lists', () => {
       .then((data) => {
         expect(data.id).to.be.above(0)
         expect(data.name).to.be.equal(LIST_NAME)
+        return ML.Lists.removeList(data.id)
+      })
+      .then(() => {
+        done()
+      })
+  })
+
+  it('should create a list, rename it, check if renamed, and remove it', (done) => {
+    ML.Lists.addList(LIST_NAME)
+      .then((data) => ML.Lists.updateList(data.id, LIST_RENAME))
+      .then((data) => {
+        expect(data.id).to.be.above(0)
+        expect(data.name).to.be.equal(LIST_RENAME)
         return ML.Lists.removeList(data.id)
       })
       .then(() => {
